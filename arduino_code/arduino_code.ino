594b48b9f617853f9040f45ce3c475c3a5a31483
V-TT <valentintapiatorti@gmail.com>
2016-12-26 23:01:03 -0300

    Metodos movimiento motores DC
    Se desarrollaron los metodos encargados de dar movimiento a los motores DC que mueven el vehiculo hacia delante, tras, izquierda y derecha.

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,11 +1,11 @@
 #include <IRLremote.h>
 
-int enA = 10;
+int enA = 10; //Conexion con motor izquierdo
 int enB = 5;
 int in1 = 9;
 int in2 = 8;
 int in3 = 7;
-int in4 = 6;
+int in4 = 6; //Conexion con motor derecho
 
 
 void setup() {
@@ -20,17 +20,74 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  adelante();
-  delay(300);
+  avanzar(200);
+  delay(2000);
+  retroceder(200);
+  delay(2000);
+  izquierda(200);
+  delay(2000);
+  derecha(200);
+  delay(2000);
 }
 
-void adelante(){
+void parar(){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  
+  analogWrite(enA, 0);
+  analogWrite(enB, 0);  
+}
+
+void avanzar(int velocidad){
+  parar();
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
   
-  analogWrite(enA, 250);
-  analogWrite(enB, 250);
+  analogWrite(enA, velocidad);
+  analogWrite(enB, velocidad);
 }
+
+void retroceder(int velocidad){
+  parar();  
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  
+  analogWrite(enA, velocidad);
+  analogWrite(enB, velocidad);  
+}
+
+void izquierda(int velocidad){  //hacemos avanzar los motores derechos 
+  parar();  
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  
+  analogWrite(enB, velocidad);
+}
+
+void derecha(int velocidad){
+  parar();
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+
+  analogWrite(enA, velocidad);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
