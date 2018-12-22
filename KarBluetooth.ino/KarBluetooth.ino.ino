730a93c7e578bc36bfd01f813edbf52ffe47af64
ValentinTapiaTorti <valentintapiatorti@gmail.com>
2018-12-22 20:31:19 -0300

    Update before changing to a new computer

--- KarBluetooth.ino/KarBluetooth.ino.ino
+++ KarBluetooth.ino/KarBluetooth.ino.ino
@@ -1,11 +1,13 @@
 #include <SoftwareSerial.h>
-
+#define pinSeguidorI A2
+#define pinSeguidorD A0
 SoftwareSerial serialBlue(11, 12); //Rx, Tx.
 int command = 0;
 int lastCommand = 0;
 int carSpeed = 150;
 
 bool automatic = false;
+bool followLine = false;
 int trig = 3;
 int echo = 2;
 int duration = 0;
@@ -16,15 +18,17 @@ int in1 = 9;
 int in2 = 8;
 int in3 = 7;
 int in4 = 6;
-int enB = 5;
+int enB = 0;
 
 void setup() {
-  // put your setup code here, to run once:  
+  // put your setup code here, to run once:
   serialBlue.begin(38400);
   Serial.begin(38400);
+  pinMode(pinSeguidorI, INPUT);
+  pinMode(pinSeguidorD, INPUT);
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
-  
+
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
@@ -37,22 +41,55 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (serialBlue.available() > 0) {
     command = serialBlue.read();
+    Serial.println((char)command);
     executeAction();
   }
   if (automatic) {
     automaticDriving();
+  } else if (followLine) {
+    followLineDriving();
   }
 }
 
 void automaticDriving() {
- getDistance();
-  if(distance > 10) {
+  getDistance();
+  if (distance >= 10) {
     up();
   } else {
     leftReverse();
-    delay(900);
-    up();
+    delay(100);
+    stopKar();
   }
+}
+
+void followLineDriving() {
+  carSpeed = 120;
+  int izq = analogRead(pinSeguidorI);
+  int der = analogRead(pinSeguidorD);
+  if (der <= 100) {
+    der = 0;
+  } else {
+    der = 1;
+  }
+  if (izq <= 100) {
+    izq = 0;
+  } else {
+    izq = 1;
+  }
+  if (der == HIGH) {
+    Serial.print("DERECHO");
+    leftLine();
+  } else if (izq == HIGH) {
+    Serial.print("IZQUIERDO");
+    RightLine();
+  } else if (izq == LOW && der == LOW) {
+    Serial.print("ADELANTE");
+    up();
+  } else {
+    Serial.print("FRENADO");
+    stopKar();
+  }
+  delay(50);
 }
 
 void getDistance() {
@@ -65,7 +102,7 @@ void getDistance() {
   digitalWrite(trig, LOW);
   duration = pulseIn(echo, HIGH);
 
-  distance = duration/58.2;
+  distance = duration / 58.2;
 }
 
 void executeAction() {
@@ -98,10 +135,16 @@ void executeAction() {
       break;
     case 'a':
       automatic = !automatic;
-      if(!automatic) stopKar(); 
+      if (!automatic) stopKar();
       else {
         carSpeed = 180;
       }
+      break;
+    case 'F':
+      followLine = !followLine;
+      if (!followLine) stopKar();
+      else
+        carSpeed = 120;
       break;
     case 'v':
       while (true) {
@@ -186,3 +229,24 @@ void stopKar() {
   digitalWrite(in4, LOW);
 }
 
+
+void leftLine() {
+  stopKar();
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+
+  analogWrite(enA, carSpeed);
+  analogWrite(enB, carSpeed);
+}
+void RightLine() {
+  stopKar();
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+
+  analogWrite(enA, carSpeed);
+  analogWrite(enB, carSpeed);
+}
